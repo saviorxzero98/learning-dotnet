c0e86034cf1dcd7741a60ee31c7ffe18a5f2bb75
saviorxzero98 <saviorxzero98@gmail.com>
2023-10-29 15:02:12 +0800

    add: Mail Template Sample

---
+++ 002-Packages/019-Mail/EmailTemplateSample/Net/FluidSample/Program.cs
@@ -0,0 +1,54 @@
+﻿using Fluid;
+
+namespace FluidSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo();
+            DemoSnakeCase();
+        }
+
+        static void Demo()
+        {
+            var parser = new FluidParser();
+
+            var model = new { Name = "World", UserName = "Liquid" };
+            var source = "Hello {{ Name }}! {{ UserName }}";
+
+            if (parser.TryParse(source, out var template, out var error))
+            {
+                var context = new TemplateContext(model);
+
+                Console.WriteLine(template.Render(context));
+            }
+            else
+            {
+                Console.WriteLine($"Error: {error}");
+            }
+        }
+
+        static void DemoSnakeCase()
+        {
+            var parser = new FluidParser();
+
+            var model = new { Name = "World", UserName = "Snake" };
+            var source = "Hello {{ name }}! {{ user_name }}";
+
+            if (parser.TryParse(source, out var template, out var error))
+            {
+                var options = new TemplateOptions();
+                options.MemberAccessStrategy.MemberNameStrategy = MemberNameStrategies.SnakeCase;
+
+                var context = new TemplateContext(model, options);
+
+                Console.WriteLine(template.Render(context));
+            }
+            else
+            {
+                Console.WriteLine($"Error: {error}");
+            }
+        }
+    }
+}
---
+++ 002-Packages/019-Mail/EmailTemplateSample/Net/ScribanSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Scriban;
+
+namespace ScribanSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoScibanRenderer();
+            DemoLiquidRenderer();
+        }
+
+        static void DemoScibanRenderer()
+        {
+            var template = Template.Parse("Hello {{name}}! {{user_name}}");
+            var result = template.Render(new { Name = "World", UserName = "Scriban" });
+            Console.WriteLine(result);
+        }
+
+        static void DemoLiquidRenderer()
+        {
+            var template = Template.ParseLiquid("Hello {{name}}! {{user_name}}");
+            var result = template.Render(new { Name = "World", UserName = "Liquid" });
+            Console.WriteLine(result);
+        }
+    }
+}
