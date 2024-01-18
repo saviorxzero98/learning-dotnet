601d0d9b71ac9283afd36864f59d6f0a49377f16
saviorxzero98 <saviorxzero98@gmail.com>
2024-01-18 18:09:55 +0800

    add: String Compress Sample

---
+++ 002-Packages/008-StringAndCypto/StringCompressSample/Net/StringCompressSample/StringCompressSample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StringCompressSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Hello, World!";
+
+            var compressor = new TextCompressor();
+            var compressText = compressor.Compress(text);
+            Console.WriteLine(compressText);
+
+            var decompressText = compressor.Decompress(compressText);
+            Console.WriteLine(decompressText);
+        }
+    }
+}
