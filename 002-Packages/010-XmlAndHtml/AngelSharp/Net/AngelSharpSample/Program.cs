7e90809da59897e68924184ba7a886630d3c0cef
saviorxzero98 <saviorxzero98@gmail.com>
2024-03-14 11:03:30 +0800

    add: AngelSharp Sample

---
+++ 002-Packages/010-XmlAndHtml/AngelSharp/Net/AngelSharpSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using AngleSharp.Dom;
+using AngleSharp;
+
+namespace AngelSharpSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //html代碼
+            var source = @"
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <meta property=""og:title"" content=""123"" />
+    <title>ABC</title>
+</head>
+</html>
+";
+
+            IDocument document = BrowsingContext.New(Configuration.Default.WithDefaultLoader())
+                                .OpenAsync(req => req.Content(source)).Result;
+
+            var title = document.QuerySelector("meta[property=\"og:title\"]")?.Attributes["content"]?.Value;
+            Console.WriteLine(title);
+        }
+    }
+}
