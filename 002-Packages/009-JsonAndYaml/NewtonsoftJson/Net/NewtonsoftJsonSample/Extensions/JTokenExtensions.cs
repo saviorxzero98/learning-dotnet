7e2d913972ed0f6058675c55ca7365fed14ec70b
saviorxzero98 <saviorxzero98@gmail.com>
2024-03-14 10:49:34 +0800

    add: Newtonsoft.Json Sample

---
+++ 002-Packages/009-JsonAndYaml/NewtonsoftJson/Net/NewtonsoftJsonSample/Extensions/JTokenExtensions.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace NewtonsoftJsonSample.Extensions
+{
+    public static class JTokenExtensions
+    {
+        /// <summary>
+        /// 檢查 JToken 是否為空
+        /// </summary>
+        /// <param name="jToken"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this JToken jToken)
+        {
+            switch (jToken.Type)
+            {
+                case JTokenType.String:
+                    string text = jToken.ToString();
+                    return string.IsNullOrEmpty(text);
+
+                case JTokenType.Array:
+                    return (((JArray)jToken).Count == 0);
+
+                case JTokenType.Object:
+                    return (((JObject)jToken).Count == 0);
+
+                case JTokenType.Undefined:
+                case JTokenType.Null:
+                case JTokenType.None:
+                    return true;
+
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Merge JToken
+        /// </summary>
+        /// <param name="jToken"></param>
+        /// <param name="appendData"></param>
+        /// <returns></returns>
+        public static JToken Merge(this JToken jToken, JToken appendData)
+        {
+            if (jToken is JObject || jToken is JArray)
+            {
+                // Merge
+                if (appendData is JObject)
+                {
+                    return jToken.MergeObject(appendData as JObject);
+                }
+                else if (appendData is JArray)
+                {
+                    return jToken.MergeArray(appendData as JArray);
+                }
+                else
+                {
+                    return jToken;
+                }
+            }
+            return jToken;
+        }
+
+        /// <summary>
+        /// Merge JObject
+        /// </summary>
+        /// <param name="jToken"></param>
+        /// <param name="appendData"></param>
+        /// <returns></returns>
+        public static JToken MergeObject(this JToken jToken, JObject appendData)
+        {
+            try
+            {
+                JToken newJToken = jToken.DeepClone();
+
+                if (newJToken is JObject)
+                {
+                    JObject jObject = newJToken as JObject;
+
+                    jObject.Merge(appendData, new JsonMergeSettings()
+                    {
+                        MergeArrayHandling = MergeArrayHandling.Union,
+                        MergeNullValueHandling = MergeNullValueHandling.Ignore
+                    });
+                    return newJToken;
+                }
+
+                if (newJToken is JArray)
+                {
+                    JArray jArray = (newJToken as JArray);
+                    jArray.Add(appendData);
+
+                    return newJToken;
+                }
+            }
+            catch
+            {
+
+            }
+            return jToken;
+        }
+
+        /// <summary>
+        /// Merge JArray
+        /// </summary>
+        /// <param name="jToken"></param>
+        /// <param name="appendData"></param>
+        /// <returns></returns>
+        public static JToken MergeArray(this JToken jToken, JArray appendData)
+        {
+            try
+            {
+                var newJToken = jToken.DeepClone();
+
+                if (newJToken is JArray)
+                {
+                    JArray jArray = (newJToken as JArray);
+
+                    jArray.Merge(appendData, new JsonMergeSettings()
+                    {
+                        MergeArrayHandling = MergeArrayHandling.Union,
+                        MergeNullValueHandling = MergeNullValueHandling.Ignore
+                    });
+                    return newJToken;
+                }
+            }
+            catch
+            {
+
+            }
+            return jToken;
+        }
+    }
+}
---
+++ 002-Packages/009-JsonAndYaml/NewtonsoftJson/Net/NewtonsoftJsonSample/Program.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq;
+using NewtonsoftJsonSample.Extensions;
+
+namespace NewtonsoftJsonSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoMergeJToken1();
+
+            DemoMergeJToken2();
+        }
+
+        static void DemoMergeJToken1()
+        {
+            // Object 1
+            var blueJToken = JToken.FromObject(new 
+            {
+                Name = "KMT",
+                Members = new string[]
+                {
+                    "Tentacool",
+                    "Angkor Wat"
+                },
+                PrimaryColor = "KMT#000095",
+                SecondaryColor = "TPP#28C8C8",
+                Detial = new
+                {
+                    Color = "#000095"
+                }
+            });
+
+            // Object 2
+            var whiteJToken = JToken.FromObject(new
+            {
+                Email = "KP@tpp.com.tw",
+                Members = new string[]
+                {
+                    "33"
+                },
+                PrimaryColor = "TPP#28C8C8",
+                SecondaryColor = "KMT#000095",
+                Detial = new
+                {
+                    Color = "#28C8C8"
+                }
+            });
+
+            // Merge Object
+            var mergeWhiteBlue = blueJToken.Merge(whiteJToken);
+            var mergeBlueWhite = whiteJToken.Merge(blueJToken);
+
+            Console.WriteLine($"Object 1:\n{blueJToken}");
+            Console.WriteLine($"Object 2:\n{whiteJToken}");
+            Console.WriteLine($"Merged Object 1:\n{mergeBlueWhite}");
+            Console.WriteLine($"Merged Object 2:\n{mergeWhiteBlue}");
+        }
+
+
+        static void DemoMergeJToken2()
+        {
+            // Array 1
+            var jtoken1 = JToken.FromObject(new string[]
+                {
+                    "Two",
+                    "Three"
+                });
+
+            // Array 2
+            var jtoken2 = JToken.FromObject(new string[]
+                {
+                    "Four",
+                    "Three"
+                });
+
+            // Merge Object
+            var mergeJToken = jtoken1.Merge(jtoken2);
+
+            Console.WriteLine($"Object 1:\n{jtoken1}");
+            Console.WriteLine($"Object 2:\n{jtoken2}");
+            Console.WriteLine($"Merged Object\n{mergeJToken}");
+        }
+    }
+}
