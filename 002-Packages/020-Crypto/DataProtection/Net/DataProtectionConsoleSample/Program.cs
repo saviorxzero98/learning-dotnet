55c1840538ba5ac0c0aefd45e06c8fef3b43e05f
saviorxzero98 <saviorxzero98@gmail.com>
2024-09-19 10:46:41 +0800

    add: DataProtection Sample

---
+++ 002-Packages/020-Crypto/DataProtection/Net/DataProtectionConsoleSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataProtectionConsoleSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = GetServiceProvider();
+            var provider = services.GetDataProtectionProvider();
+
+
+            var dataProtector = provider.CreateProtector("Sample");
+
+            var text = "Hello World";
+            var secretText = dataProtector.Protect(text);
+            var plainText = dataProtector.Unprotect(secretText);
+
+            Console.WriteLine($"Text: {text}");
+            Console.WriteLine($"Secret Text: {secretText}");
+            Console.WriteLine($"Plain Text: {plainText}");
+        }
+
+
+        static IServiceProvider GetServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo("Keys"));
+            return services.BuildServiceProvider();
+        }
+    }
+}
