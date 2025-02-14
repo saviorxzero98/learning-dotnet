84711519f90217f2b7386a3f3ec57d225c511881
saviorxzero98 <saviorxzero98@gmail.com>
2025-02-14 18:18:51 +0800

    update: ABP Framework WebAPI Sample

---
+++ 012-Framework/AbpFramework/AbpWebApiSample/test/AbpWebApiSample.TestBase/AbpWebApiSampleTestBase.cs
@@ -0,0 +1,24 @@
+﻿using AbpWebApiSample.Domain;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+using Volo.Abp.Uow;
+
+namespace AbpWebApiSample.TestBase
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(AbpTestBaseModule),
+        typeof(AbpWebApiSampleDomainModule)
+    )]
+    public class AbpWebApiSampleTestBase : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpUnitOfWorkDefaultOptions>(options =>
+            {
+                options.TransactionBehavior = UnitOfWorkTransactionBehavior.Disabled;
+            });
+        }
+    }
+}
