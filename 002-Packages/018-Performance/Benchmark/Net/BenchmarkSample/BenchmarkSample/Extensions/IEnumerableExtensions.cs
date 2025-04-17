437dae2da128fa10826fff4bb4e1a97fc682511f
saviorxzero98 <saviorxzero98@gmail.com>
2025-04-17 15:41:21 +0800

    add: Benchmark Sample

---
+++ 002-Packages/018-Performance/Benchmark/Net/BenchmarkSample/BenchmarkSample/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace BenchmarkSample.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        // can be removed in .NET 6 because it's built-in https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.parallel.foreachasync?view=net-6.0
+        public static Task ParallelForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> body)
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = DataflowBlockOptions.Unbounded
+            };
+
+            var block = new ActionBlock<T>(body, options);
+            foreach (var item in source)
+            {
+                block.Post(item);
+            }
+
+            block.Complete();
+            return block.Completion;
+        }
+
+        // can be removed in .NET 6 because it's built-in https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.parallel.foreachasync?view=net-6.0
+        public static Task ParallelForEachAsync<T>(this IEnumerable<T> source, ParallelOptions parallelOptions,
+                                                   Func<T, Task> body)
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = parallelOptions.MaxDegreeOfParallelism
+            };
+
+            if (parallelOptions.TaskScheduler != null)
+            {
+                options.TaskScheduler = parallelOptions.TaskScheduler;
+            }
+
+            var block = new ActionBlock<T>(body, options);
+
+            foreach (var item in source)
+            {
+                block.Post(item);
+            }
+
+            block.Complete();
+            return block.Completion;
+        }
+    }
+}
---
+++ 002-Packages/018-Performance/Benchmark/Net/BenchmarkSample/BenchmarkSample/ParallelForEachCalculator.cs
@@ -0,0 +1,129 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkSample.Extensions;
+using System.Collections.Concurrent;
+
+namespace BenchmarkSample
+{
+    public class ParallelForEachCalculator
+    {
+        public List<string> TestData { get; set; }
+        public int Threads { get; set; } = 4;
+
+        public ParallelForEachCalculator() 
+        {
+            TestData = new List<string>();
+            for(int i = 0; i < 100; i++)
+            {
+                TestData.Add(Guid.NewGuid().ToString());
+            }
+        }
+
+
+        [Benchmark]
+        public List<string> ProcessBySignle()
+        {
+            List<string> results = new List<string>();
+            foreach (var item in TestData)
+            {
+                if (ExecuteProcess(item))
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+
+        [Benchmark]
+        public List<string> ProcessByParallel()
+        {
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = (Threads > 0) ? Threads : Environment.ProcessorCount
+            };
+
+            var results = new ConcurrentStack<string>();
+            Parallel.ForEach(TestData, options, (item, loopState) =>
+            {
+                if (ExecuteProcess(item))
+                {
+                    results.Push(item);
+                }
+            });
+            return results.ToList();
+        }
+
+        [Benchmark]
+        public async Task<List<string>> ProcessByParallelAsync()
+        {
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = (Threads > 0) ? Threads : Environment.ProcessorCount
+            };
+
+            var results = new ConcurrentStack<string>();
+            await Parallel.ForEachAsync(TestData, options, async (item, loopState) => {
+                if (await ExecuteProcessAsync(item))
+                {
+                    results.Push(item);
+                }
+            });
+            return results.ToList();
+        }
+
+        [Benchmark]
+        public List<string> ProcessByPlinq()
+        {
+            int degreeOfParallelism = (Threads > 0) ? Threads : Environment.ProcessorCount;
+            var results = new ConcurrentStack<string>();
+            TestData.AsParallel()
+                    .WithDegreeOfParallelism(degreeOfParallelism)
+                    .ForAll(item =>
+                    {
+                        if (ExecuteProcess(item))
+                        {
+                            results.Push(item);
+                        }
+                    });
+            return results.ToList();
+        }
+
+        [Benchmark]
+        public async Task<List<string>> ProcessByDataFlowAsync()
+        {
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = (Threads > 0) ? Threads : Environment.ProcessorCount
+            };
+
+            var results = new ConcurrentStack<string>();
+            await TestData.ParallelForEachAsync(options, async (item) =>
+            {
+                if (await ExecuteProcessAsync(item))
+                {
+                    results.Push(item);
+                }
+            });
+            return results.ToList();
+        }
+
+        private bool ExecuteProcess(string item)
+        {
+            Thread.Sleep(1);
+            if (item.Contains("6") || item.Contains("5"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private Task<bool> ExecuteProcessAsync(string item)
+        {
+            Thread.Sleep(1);
+            if (item.Contains("6") || item.Contains("5"))
+            {
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+    }
+}
